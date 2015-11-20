151eb941f745eb5f8b38858121a9aae056dedab8
TheoWinterhalter <isheeft@gmail.com>
2015-11-20 01:14:54 +0100

    Collapsable menu for modules

--- script/doc.js
+++ script/doc.js
@@ -1,6 +1,8 @@
 $(function () {
 
+  //////////////////////////
   // Handles all doc drawers
+  //////////////////////////
 
   $('.more').hide();
 
@@ -9,8 +11,6 @@ $(function () {
   $('.showmore').click(function () {
     var divmore = $(this).parent().children('.more');
     var arrow   = $(this).parent().children('.arrow');
-    // arrow.toggleClass('arrow-right');
-    // arrow.toggleClass('arrow-bottom');
     arrow.toggleClass('rotate');
     if(divmore.is(':hidden')) {
       divmore.slideDown("fast");
@@ -20,7 +20,45 @@ $(function () {
     }
   });
 
+  ///////////////////////
+  // Collapsable nav tree
+  ///////////////////////
+
+  $('aside ul').hide();
+  $('#main-nav').show();
+
+  // We remove the arrow when the li has no ul inside
+  $('aside li:not(:has(ul ul))').children('.arrow').hide();
+
+  $('.shownav').click(function () {
+    var ul    = $(this).parent().children('ul');
+    var arrow = $(this);
+    arrow.toggleClass('rotate');
+    if(ul.is(':hidden')) {
+      ul.slideDown("fast");
+    }
+    else {
+      ul.slideUp("fast");
+    }
+  });
+
+  // Show all button
+  $('.showall:not(.hideall)').click(function () {
+    $('.shownav:not(.rotate)').trigger('click');
+    $(this).text('Hide all');
+    $(this).addClass('hideall');
+  });
+
+  // Hide all button
+  $('.showall.hideall').click(function () {
+    $('.shownav.rotate').trigger('click');
+    $(this).text('Show all');
+    $(this).removeClass('hideall');
+  });
+
+  /////////////////////////////////
   // Handles menu for small screens
+  /////////////////////////////////
 
   $('#nav-trigger').click(function () {
     $('aside').toggleClass('nav-visible');
